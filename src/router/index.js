c056f9836bc7273ba4752cac96f8a5d10095c1e1
yamapi0103 <yamapi0103@gmail.com>
2021-04-05 01:43:17 +0800

    Securing routes (清除使用者資訊會導到登入頁)

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Chat from '../pages/Chat'
 import Login from '../pages/Login'
-
+import auth from 'firebase/auth'
 
 Vue.use(Router)
 
@@ -11,7 +11,14 @@ export default new Router({
     {
       path: '/',
       name: 'chat',
-      component: Chat
+      component: Chat,
+      beforeEnter: (to, from, next) => {
+        if (!firebase.auth().currentUser) {
+          next('/login')
+        } else {
+          next()
+        }
+      }
     },
     {
       path: '/login',
