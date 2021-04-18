e98e91a721a7b29ca0cd76f0dec4f3328c504949
yamapi0103 <yamapi0103@gmail.com>
2021-04-18 19:50:13 +0800

    data filter and self styling

--- src/messages/SingleMessage.vue
+++ src/messages/SingleMessage.vue
@@ -3,13 +3,20 @@
     <div class="mt-3">
       <div v-for="(message, index) in messages" :key="index">
         <div class="media">
-          <img :src="message.user.avatar" height="50" class="align-self-start mr-3" />
+          <img
+            :src="message.user.avatar"
+            height="50"
+            class="align-self-start mr-3"
+          />
 
           <div class="media-body">
             <h6 class="mt-0">
-              <a href="#">{{ message.user.name }}</a> - {{ message.timestamp }}
+              <a href="#">{{ message.user.name }}</a> -
+              {{ message.timestamp | fromNow }}
             </h6>
-            <p>{{ message.content }}</p>
+            <p :class="{ self_message: selfMessage(message.user) }">
+              {{ message.content }}
+            </p>
           </div>
         </div>
       </div>
@@ -18,8 +25,29 @@
 </template>
 
 <script>
+  import moment from "moment";
+  import { mapGetters } from "vuex";
   export default {
     name: "single-message",
     props: ["messages"],
+    computed: {
+      ...mapGetters(["currentUser"]),
+    },
+    filters: {
+      fromNow(val) {
+        return moment(val).fromNow();
+      },
+    },
+    methods: {
+      selfMessage(user) {
+        return user.id === this.currentUser.uid;
+      },
+    },
   };
 </script>
+<style scoped>
+  .self_message{
+    border-left: 5px solid red;
+    padding: 0 10px;
+  }
+</style>
