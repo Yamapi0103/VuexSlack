c1bead6984d0d6009b4533cc3c0a3970b912d426
yamapi0103 <yamapi0103@gmail.com>
2021-04-21 22:54:36 +0800

    online/offline status

--- src/sidebar/Users.vue
+++ src/sidebar/Users.vue
@@ -6,7 +6,13 @@
         <li v-for="user in users" :key="user.uid">
           <span>
             <img :src="user.avatar" height="20" class="img rounded-circle" />
-            <span class="text-primary">{{ user.name }}</span>
+            <span
+              :class="{
+                'text-primary': isOnline(user),
+                'text-danger': !isOnline(user),
+              }"
+              >{{ user.name }}</span
+            >
           </span>
         </li>
       </ul>
@@ -33,11 +39,26 @@
     methods: {
       addListeners() {
         this.usersRef.on("child_added", (snapshot) => {
+          console.log(snapshot);
           if (this.currentUser.uid !== snapshot.key) {
-            let user = snapshot.val();
-            user["uid"] = snapshot.key;
-            user["status"] = "offline";
-            this.users.push(user);
+          let user = snapshot.val();
+          user["uid"] = snapshot.key;
+          user["status"] = "offline";
+          this.users.push(user);
+          }
+        });
+
+        // presenceRef child_added
+        this.presenceRef.on("child_added", (snapshot) => {
+          if (this.currentUser.uid !== snapshot.key) {
+            this.addStatusToUser(snapshot.key);
+          }
+        });
+
+        // presenceRef child_removed
+        this.presenceRef.on("child_removed", (snapshot) => {
+          if (this.currentUser.uid !== snapshot.key) {
+            this.addStatusToUser(snapshot.key, false);
           }
         });
 
@@ -50,7 +71,22 @@
           }
         });
       },
-      detachListeners() {},
+      addStatusToUser(userId, connected = true) {
+        let index = this.users.findIndex((user) => user.uid === userId);
+        if (index !== -1) {
+          connected
+            ? (this.users[index].status = "online")
+            : (this.users[index].status = "offline");
+        }
+      },
+      isOnline(user) {
+        return user.status == "online";
+      },
+      detachListeners() {
+        this.usersRef.off();
+        this.connectedRef.off();
+        this.presenceRef.off();
+      },
     },
     mounted() {
       this.addListeners();
